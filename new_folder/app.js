c23ad053a475c86ced09b5dde4aa6d3129b4a5f9
Vaishnavi warad <vaishnaviwarad21@gmail.com>
2026-05-21 20:00:05 +0530

    Delete new_folder directory

--- new_folder/app.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const app = express();
-
-app.get("/hello", (req, res) => {
-  res.status(200).json({ message: "Hello World" });
-});
-
-module.exports = app;
--- new_folder/index.js
+++
@@ -1,9 +0,0 @@
-
-const app = require("./app");
-
-const PORT =  4000;
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
